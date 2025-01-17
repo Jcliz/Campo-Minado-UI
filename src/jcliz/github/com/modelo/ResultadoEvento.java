bf91083867e9e2f60f3410647bcb7511773b2765
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2025-01-17 10:03:05 -0300

    Padronização Observer

---
+++ src/jcliz/github/com/modelo/ResultadoEvento.java
@@ -0,0 +1,13 @@
+package jcliz.github.com.modelo;
+
+public class ResultadoEvento {
+    private final boolean ganhou;
+
+    public boolean isGanhou() {
+        return ganhou;
+    }
+
+    public ResultadoEvento(boolean ganhou) {
+        this.ganhou = ganhou;
+    }
+}
--- src/jcliz/github/com/modelo/Tabuleiro.java
+++ src/jcliz/github/com/modelo/Tabuleiro.java
@@ -2,14 +2,17 @@ package jcliz.github.com.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
-public class Tabuleiro {
+public class Tabuleiro implements CampoObservador {
     private int linhas;
     private int colunas;
     private int minas;
 
     private final List<Campo> campos = new ArrayList<>();
+    private final List<Consumer<ResultadoEvento>> observadores =
+            new ArrayList<>();
 
     public Tabuleiro(int linhas, int colunas, int minas) {
         this.linhas = linhas;
@@ -22,19 +25,22 @@ public class Tabuleiro {
         sortearMinas();
     }
 
+    public void registrarObservador(Consumer<ResultadoEvento> observador) {
+        observadores.add(observador);
+    }
+
+    private void notificarObservadores(boolean resultado) {
+        observadores.stream()
+                .forEach(o -> o.accept(new ResultadoEvento(resultado)));
+    }
+
     public void abrir(int linha, int coluna) {
-        try {
-            campos.parallelStream()
-                    //filtra os campos para encontrar o desejado com os parametros
-                    .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
-                    .findFirst()
-                    //caso no método abrir a explosão ser jogada, cairá no catch
-                    .ifPresent(Campo::abrir);
-        } catch (Exception e) {
-            //FIXME Ajustar a implementação do método abrir
-            campos.forEach(c -> c.setAberto(true));
-            throw e;
-        }
+        campos.parallelStream()
+                //filtra os campos para encontrar o desejado com os parametros
+                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                .findFirst()
+                //caso no método abrir a explosão ser jogada, cairá no catch
+                .ifPresent(Campo::abrir);
     }
 
     public void marcar(int linha, int coluna) {
@@ -49,7 +55,9 @@ public class Tabuleiro {
         //geração de campos com uma geração de matriz
         for (int i = 0; i < linhas; i++) {
             for (int j = 0; j < colunas; j++) {
-                campos.add(new Campo(i, j));
+                Campo campo = new Campo(i, j);
+                campo.registrarObservador(this);
+                campos.add(campo);
             }
         }
     }
@@ -85,6 +93,22 @@ public class Tabuleiro {
     public void reiniciar() {
         campos.forEach(Campo::reiniciar);
         sortearMinas();
+    }
+
+    @Override
+    public void eventoOcorreu (Campo campo, CampoEvento evento) {
+        if (evento == CampoEvento.EXPLODIR) {
+            mostrarMinas();
+            notificarObservadores(false);
+        } else if (objetivoAlcancado()) {
+            notificarObservadores(true);
+        }
+    }
+
+    private void mostrarMinas() {
+        campos.stream()
+                .filter(Campo::isMinado)
+                .forEach(c -> c.setAberto(true));
     }
 
     public List<Campo> getCampos() {
