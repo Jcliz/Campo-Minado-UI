431a3a6e704fb451a48d697b679978760fec0029
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2025-01-17 10:30:01 -0300

    Criação da classe para o botão

---
+++ src/jcliz/github/com/visao/BotaoCampo.java
@@ -0,0 +1,11 @@
+package jcliz.github.com.visao;
+
+import jcliz.github.com.modelo.Campo;
+
+import javax.swing.*;
+
+public class BotaoCampo extends JButton {
+    public BotaoCampo(Campo campo) {
+
+    }
+}
