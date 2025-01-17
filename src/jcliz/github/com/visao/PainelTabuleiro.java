846a01f5645fcc00d00bc8e99291210d6654e3e5
João Pedro Cardoso de Liz <joaopedrocliz@gmail.com>
2025-01-17 10:20:08 -0300

    Início da interface gráfica

---
+++ src/jcliz/github/com/visao/PainelTabuleiro.java
@@ -0,0 +1,11 @@
+package jcliz.github.com.visao;
+
+import jcliz.github.com.modelo.Tabuleiro;
+
+import javax.swing.*;
+
+public class PainelTabuleiro extends JPanel {
+    public PainelTabuleiro(Tabuleiro tabuleiro) {
+
+    }
+}
